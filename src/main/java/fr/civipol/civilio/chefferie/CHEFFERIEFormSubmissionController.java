94f55a571abead40c1812996d16d990e51b7386a
Poupson <fonkamchristian1@gmail.com>
2025-05-06 23:09:20 +0000

    formchefferie

--- src/main/java/fr/civipol/civilio/chefferie/CHEFFERIEFormSubmissionController.java
+++ src/main/java/fr/civipol/civilio/chefferie/CHEFFERIEFormSubmissionController.java
@@ -1,4 +1,17 @@
 package fr.civipol.civilio.chefferie;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class CHEFFERIEFormSubmissionController {
+
+    @FXML
+    void onDiscardButtonClicked(ActionEvent ignored) {
+
+    }
+
+    @FXML
+    void onSubmitButtonClicked(ActionEvent ignored) {
+
+    }
 }
