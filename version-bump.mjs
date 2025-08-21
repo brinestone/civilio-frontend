4b37403d4df65f8b4e773d683d967802e580719b
Conrad Bekondo <conradbekondo@gmail.com>
2025-08-22 00:18:55 +0100

    chore: add version bumper

--- version-bump.mjs
+++ version-bump.mjs
@@ -1,31 +1,12 @@
-import {readFile, writeFile} from 'node:fs/promises';
-import {join} from 'node:path';
-import {exec} from 'node:child_process';
-
-const file = join(import.meta.dirname, 'build.gradle.kts');
-
-async function updateVersion() {
-    try {
-        const projectInfo = await readFile('package.json', 'utf8');
-        const {version} = JSON.parse(projectInfo);
-
-        const buildGradleContent = await readFile(file, 'utf8');
-        const updatedContent = buildGradleContent.replace(/^\s*version\s*=\s*".*"/m, `version = "${version}"`);
-        await writeFile(file, updatedContent, 'utf8');
-
-        console.log(`Updated version to ${version} in build.gradle.kts. Amending previous commit`);
-        exec('git add .', (error) => {
-            if (error) {
-                console.error(error);
-                return;
-            }
-
-            exec('git commit --amend --no-edit')
-        })
-    } catch (e) {
-        console.error('Error reading version:', e);
-        throw e;
+export function readVersion(contents) {
+    const versionMatch = contents.match(/version\s*=\s*["']([^"']+)["']/);
+    if (versionMatch) {
+        return versionMatch[1];
     }
+    throw new Error("Version not found in the file.");
 }
 
-updateVersion();
+export function writeVersion(contents, version) {
+    const newContents = contents.replace(/version\s*=\s*["'][^"']+["']/m, `version = "${version}"`);
+    return newContents;
+}
